99cbafbe0de6b04cd71917d9f1561ee3bc94ee8a
jbp173 <jbp173@gmail.com>
2018-04-30 22:51:36 -0600

    Added Markers documentation, made Excalibur.py an endless loop

--- Excalibur.py
+++ Excalibur.py
@@ -11,44 +11,45 @@ Code adapted from snipits found at: http://www.philipzucker.com/aruco-in-opencv/
 import cv2
 import cv2.aruco as aruco
 
-#Read input from same directory as this file
-image = cv2.imread('./Uploads/camelot-image.png')
+while (True):
+    #Read input from same directory as this file
+    image = cv2.imread('./Uploads/camelot-image.png')
 
-# Operations on the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Operations on the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#AruCo dictionary/parameters is static, we just need to declare it to access it
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-parameters =  aruco.DetectorParameters_create()
+    #AruCo dictionary/parameters is static, we just need to declare it to access it
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters =  aruco.DetectorParameters_create()
 
-#Lists of ids and the corners beloning to each id, we're only using the ids
-corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    #Lists of ids and the corners beloning to each id, we're only using the ids
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
-#Generate output and write to file    
-output = ""    
-total = 0
+    #Generate output and write to file    
+    output = ""    
+    total = 0
 
-for i in range(1,9):
-    if [i] not in ids:
-        output += str(i-5) + ", " #-5 is used because ids start at 1, but our Excalibur positions start at -4
-        total += 1
+    for i in range(1,9):
+        if [i] not in ids:
+            output += str(i-5) + ", " #-5 is used because ids start at 1, but our Excalibur positions start at -4
+            total += 1
 
-if total == 0:
-    output = "All markers found, Excalibur is not placed correctly."
-elif total == 9:
-    output = "No markers found, please retake picture."
-elif total >= 4:
-    output = "Please retake picture, photo is not centered."
-elif total == 1:
-    output = "Excalibur is in position " + output
-else:
-    if output[len(output)-4] == "-":
-        output = "Excalibur is one of positions " + output[:len(output)-4] + "or " + output[len(output)-4:len(output)-2]
+    if total == 0:
+        output = "All markers found, Excalibur is not placed correctly."
+    elif total == 9:
+        output = "No markers found, please retake picture."
+    elif total >= 4:
+        output = "Please retake picture, photo is not centered."
+    elif total == 1:
+        output = "Excalibur is in position " + output
     else:
-        output = "Excalibur is one of positions " + output[:len(output)-3] + "or " + output[len(output)-3]
+        if output[len(output)-4] == "-":
+            output = "Excalibur is one of positions " + output[:len(output)-4] + "or " + output[len(output)-4:len(output)-2]
+        else:
+            output = "Excalibur is one of positions " + output[:len(output)-3] + "or " + output[len(output)-3]
 
-#print(output)
+    #print(output)
 
-f = open("myFile.txt", "w")
-f.write(output)
+    f = open("myFile.txt", "w")
+    f.write(output)
 f.close()
